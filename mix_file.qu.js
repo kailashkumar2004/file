a5861f4adf593878e8ae49a84e48d101f0358e7e
kailash <kailashkumartkg@gmail.com>
2023-07-27 22:58:11 +0530

    initial commit

---
+++ mix_file.qu.js
@@ -0,0 +1,49 @@
+// var file = require("fs");
+// file.writeFile("kailash.text", "i am come bact to home", function (err) {
+//     if (err) {
+//         console.log("err==================",err)
+//     }
+//     else {
+//         console.log("your data save seccussefully")
+//     }
+// })
+
+// let file = require("fs");
+// file.readFile("kailash.text", "utf8", function (err, result) {
+//     if (err) {
+//         console.log("err==================",err)
+//     }
+//     else {
+//         console.log("i am kailash",result)
+//     }
+// })
+
+// const file = require("fs")
+// file.appendFile("kailash.text", "+ i love you baby", function (err) {
+//     if (err) {
+//         console.log("err======================",err)
+//     }
+//     else {
+//         console.log("welcome to my cary kailas.text")
+//     }
+// })
+
+// var file = require("fs");
+// file.unlink("kailash.text", function (err){
+//     if (err) {
+//         console.log("err=================",err)
+//     }
+//     else {
+//         console.log("your data delete seuccessfully")
+//     }
+// })
+
+// var file = require("fs");
+// file.rename("helo.text", "Helogi.text", function (err) {
+//     if (err) {
+//         console.log("err==============",err)
+//     }
+//     else {
+//         console.log("your data rename successfully")
+//     }
+// })
